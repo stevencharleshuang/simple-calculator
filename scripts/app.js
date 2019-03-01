e9e8133277dd37366e0fee4b05f2c5958db20f29
Steven Huang <steven.charles.huang@gmail.com>
2019-03-01 15:12:57 -0500

    added basic logic and functionality for addition

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,55 @@
 $(document).ready(() => {
-  console.log('jQ up and running!');
+  // jQuery DOM element selector vars
+  const
+    $btn = $('.btn'),
+    $btnEquals = $('.btn-equals'),
+    $btnNum = $('.btn-num'),
+    $btnOp = $('.btn-op'),
+    $display = $('.display');
+  
+  let
+    numStr = '',
+    newNumStr = '',
+    eqDisplay = '',
+    operator = '',
+    equation = [];
+
+  const calculate = (a, op, b) => {
+    switch (op) {
+      case '+':
+        return parseInt(a) + parseInt(b);
+      default:
+    }
+  };
+  
+  // Button Click Handler
+  $btnNum.click((e) => {
+    if (operator !== '') {
+      newNumStr += e.target.dataset.id;
+      eqDisplay = `${equation[0]} ${equation[1]} ${newNumStr}`
+      $display.text(eqDisplay)
+    } else {
+      numStr += e.target.dataset.id;
+      $display.text(numStr);
+    }
+  });
+
+  $btnOp.click((e) => {
+    if (numStr !== '') {
+      operator = e.target.dataset.id;
+      equation.push(numStr, operator);
+      console.log({equation});
+      eqDisplay = `${numStr} ${operator} `
+      $display.text(eqDisplay);
+    }
+  });
+
+  $btnEquals.click(() => {
+    if (equation.length = 2) {
+      equation.push(newNumStr);
+      console.log(equation);
+      let result = calculate(...equation);
+      console.log(result);
+    }
+  });
 });
